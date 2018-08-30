9349ba2fd9b809528a55378be9a5fbbf62f85675
Jim Andreasen <reifnir@gmail.com>
2018-08-30 06:42:38 -0400

    completed exersise 3

--- and101/Exercise 3/Me/TipCalculator/MainActivity.cs
+++ and101/Exercise 3/Me/TipCalculator/MainActivity.cs
@@ -1,16 +1,39 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace TipCalculator
 {
 	[Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		protected override void OnCreate(Bundle savedInstanceState)
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.Main);
-		}
-	}
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+            calculateButton.Click += OnCalculateClick;
+        }
+
+        private void OnCalculateClick(object sender, System.EventArgs e)
+        {
+            var billText = inputBill.Text;
+            var billTotal = double.Parse(billText);
+
+            var tip = Math.Round((billTotal * 0.2), 2);
+            var totalPayment = billTotal + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = totalPayment.ToString();
+        }
+    }
 }
