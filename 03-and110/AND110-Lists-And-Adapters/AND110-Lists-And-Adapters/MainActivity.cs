31c8ee1904b0cc1dd9693a3bbec30f94f28336d4
Jim Andreasen <reifnir@gmail.com>
2018-09-01 15:58:29 -0400

    ex 2 complete

--- 03-and110/AND110-Lists-And-Adapters/AND110-Lists-And-Adapters/MainActivity.cs
+++ 03-and110/AND110-Lists-And-Adapters/AND110-Lists-And-Adapters/MainActivity.cs
@@ -2,6 +2,8 @@
 using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
+using Android.Content;
+using System;
 
 namespace AND110ListsAndAdapters
 {
@@ -16,11 +18,11 @@ namespace AND110ListsAndAdapters
             SetContentView(Resource.Layout.Main);
 
             var list = FindViewById<ListView>(Resource.Id.instructorViewList);
-            var instructors = new List<Instructor>();
-            instructors.AddRange(InstructorData.Instructors);
-            instructors.AddRange(InstructorData.Instructors);
-            var adapter = new ArrayAdapter<Instructor>(this, Android.Resource.Layout.SimpleListItem1, instructors);
+
+            var adapter = new ArrayAdapter<Instructor>(this, Android.Resource.Layout.SimpleListItem1, InstructorData.Instructors);
             list.Adapter = adapter;
+
+            list.ItemClick += List_ItemClick;
 
             //copy of the instructors list
             //var instructors = InstructorData.Instructors.ToList();
@@ -28,6 +30,18 @@ namespace AND110ListsAndAdapters
             //list.Adapter = adapter;
 
         }
+
+        void List_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var alert = new AlertDialog.Builder(this);
+
+            var position = e.Position;
+
+            alert.SetMessage(InstructorData.Instructors[position].ToString());
+            alert.SetNeutralButton("OK", delegate { });
+            alert.Show();
+        }
+
     }
 }
 
