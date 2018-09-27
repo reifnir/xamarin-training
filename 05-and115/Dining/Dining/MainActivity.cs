bd5f696bcf4f42df69c2319c1a8659409bdd73e3
Jim Andreasen <reifnir@gmail.com>
2018-09-27 06:51:38 -0400

    ready for exercise 1 of AND115 - RecyclerView and CardView

--- 05-and115/Dining/Dining/MainActivity.cs
+++ 05-and115/Dining/Dining/MainActivity.cs
@@ -2,56 +2,27 @@
 using Android.App;
 using Android.OS;
 using Android.Runtime;
-using Android.Support.Design.Widget;
-using Android.Support.V7.App;
+//using Android.Support.Design.Widget;
+//using Android.Support.V7.App;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
 
 namespace Dining
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "@string/app_name", /*Theme = "@style/AppTheme.NoActionBar",*/ MainLauncher = true)]
+    public class MainActivity : Activity
     {
         RecyclerView recyclerView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
-
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
         }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-	}
+    }
 }
 
